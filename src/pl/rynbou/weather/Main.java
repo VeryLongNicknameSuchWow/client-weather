91135529df0cfe3f3fe494e4a08df0764a4fb7bd
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2018-12-11 21:29:49 +0100

    simple window solution

--- src/pl/rynbou/weather/Main.java
+++ src/pl/rynbou/weather/Main.java
@@ -1,31 +1,9 @@
 package pl.rynbou.weather;
 
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
+import pl.rynbou.weather.window.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
-        Datetime t = new Datetime();
-
-        Reading.reloadReadings();
-        List<Reading> lastReadings = new ArrayList<>();
-        lastReadings.add(Reading.closestReading(Reading.Location.DOWNSTAIRS, t.getDate("UTC")));
-        lastReadings.add(Reading.closestReading(Reading.Location.OUTSIDE, t.getDate("UTC")));
-        lastReadings.add(Reading.closestReading(Reading.Location.UPSTAIRS, t.getDate("UTC")));
-
-        for (Reading r : lastReadings) {
-            System.out.println(r.getLocation());
-            System.out.println(r.getTimestamp());
-            System.out.println("T: " + r.getTemperature());
-            if (r.getLocation().equals(Reading.Location.DOWNSTAIRS) || r.getLocation().equals(Reading.Location.UPSTAIRS))
-                System.out.println("H: " + r.getHumidity());
-            if (r.getLocation().equals(Reading.Location.UPSTAIRS))
-                System.out.println("P: " + r.getPressure());
-        }
-
-        System.out.println(t.getDate(ZoneId.systemDefault()));
-
-        //MainWindow.launch();
+        MainWindow.launch();
     }
 }
--- src/pl/rynbou/weather/window/MainWindow.java
+++ src/pl/rynbou/weather/window/MainWindow.java
@@ -1,25 +1,68 @@
 package pl.rynbou.weather.window;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import pl.rynbou.weather.Datetime;
+import pl.rynbou.weather.Reading;
 
 import java.io.FileInputStream;
 
+//TODO: Rewrite
 public class MainWindow extends Application {
+    @FXML
+    Button button;
+    @FXML
+    Label label0, label1, label2;
+
+    @FXML
+    private void initialize() {
+        update();
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         String path = "fxmls/MainWindow.fxml";
         FileInputStream stream = new FileInputStream(path);
-        VBox root = (VBox) loader.load(stream);
+        Pane root = loader.load(stream);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("JD");
+        stage.setTitle("Weather client");
         stage.show();
+    }
+
+    public void update() {
+        Datetime t = new Datetime();
+
+        Reading.reloadReadings();
+        Reading downstairs = Reading.closestReading(Reading.Location.DOWNSTAIRS, t.getDate("UTC"));
+        Reading outside = Reading.closestReading(Reading.Location.OUTSIDE, t.getDate("UTC"));
+        Reading upstairs = Reading.closestReading(Reading.Location.UPSTAIRS, t.getDate("UTC"));
+
+        label0.setText(
+                "OUTSIDE\n" +
+                        "T: " + outside.getTemperature() + "°C\n" +
+                        "P: " + upstairs.getPressure() + "\n" +
+                        outside.getTimestamp());
+
+        label1.setText(
+                "UPSTAIRS\n" +
+                        "T: " + upstairs.getTemperature() + "°C\n" +
+                        "H: " + upstairs.getHumidity() + "%\n" +
+                        upstairs.getTimestamp());
+
+        label2.setText(
+                "DOWNSTAIRS\n" +
+                        "T: " + downstairs.getTemperature() + "°C\n" +
+                        "H: " + downstairs.getHumidity() + "%\n" +
+                        downstairs.getTimestamp());
     }
 
     public static void launch() {
